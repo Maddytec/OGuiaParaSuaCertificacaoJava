d154863595ea33f6907a1ef7d0b019cdf65e5b7b
Madson Silva <maddytec@gmail.com>
2016-09-05 16:17:35 -0300

    Exercicio 04 resolvido

---
+++ CertificacaoJavaProgrammerI/src/Exercicio04.java
@@ -0,0 +1,23 @@
+/*
+ * 4) Escolha a opção adequada ao tentar compilar e rodar o código a seguir:
+ */
+
+public class Exercicio04 {
+	static int x = 15;
+	
+	public static void main(String[] args) {
+		x=200;
+		System.out.println(x);
+	}
+}
+
+/*
+ * a) O código compila e roda, imprimindo 200.
+ * b) O código compila e roda, imprimindo 15. 
+ * c) O código não compila.
+ * d) O código compila mas dá erro em execução.
+ 
+ *
+ *Resposta: letra a, a variável estatica x é da classe e pode ser usada por toda a classe. 
+ */
+
